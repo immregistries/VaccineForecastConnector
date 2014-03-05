d589d5612ae108f761cf974589c29a1fb370bf58
Nathan Bunker <122847+nathanbunker@users.noreply.github.com>
2014-03-05 11:50:39 +0000

    Added handling assuming adults up-to-date

--- src/main/java/org/tch/fc/model/Admin.java
+++ src/main/java/org/tch/fc/model/Admin.java
@@ -5,7 +5,8 @@ import java.io.Serializable;
 public enum Admin implements Serializable {
   DUE("D", "Due"), OVERDUE("O", "Overdue"), DUE_LATER("L", "Due Later"), CONTRAINDICATED("X", "Contraindicated"), COMPLETE(
       "C", "Complete"), COMPLETE_FOR_SEASON("S", "Complete For Season"), NOT_COMPLETE("N", "Not Complete"), FINISHED(
-      "F", "Finished"), IMMUNE("I", "Immune"), UNKNOWN("U", "Unknown"), ERROR("E", "Error"), NO_RESULTS("R", "No Results");
+      "F", "Finished"), IMMUNE("I", "Immune"), UNKNOWN("U", "Unknown"), ERROR("E", "Error"), NO_RESULTS("R",
+      "No Results"), ASSUMED_COMPLETE_OR_IMMUNE("A", "Assumed Complete or Immune");
 
   private Admin(String adminStatus, String label) {
     this.adminStatus = adminStatus;
