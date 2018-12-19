6efe83ff1977bc087bd85a577f26c9393cf9e861
Tamri, Hossam (IntlAssoc) <hossam.tamri@nist.gov>
2018-12-19 10:40:15 -0500

    Update MDS Connector code

--- src/main/java/org/tch/fc/MDSConnector.java
+++ src/main/java/org/tch/fc/MDSConnector.java
@@ -12,9 +12,7 @@ import java.util.Map;
 import javax.xml.datatype.XMLGregorianCalendar;
 import org.tch.fc.model.Evaluation;
 import org.tch.fc.model.EvaluationActual;
-import org.tch.fc.model.Event;
 import org.tch.fc.model.ForecastActual;
-import org.tch.fc.model.Service;
 import org.tch.fc.model.Software;
 import org.tch.fc.model.SoftwareResult;
 import org.tch.fc.model.SoftwareResultStatus;
@@ -22,6 +20,7 @@ import org.tch.fc.model.TestCase;
 import org.tch.fc.model.TestEvent;
 import org.tch.fc.model.VaccineGroup;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import gov.nist.healthcare.iz.cds.swp.mds.client.domain.Dose;
@@ -37,8 +36,9 @@ public class MDSConnector implements ConnectorInterface {
     private Software software = null;
     static SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy");
     boolean logText = false;
+    private ObjectMapper mapper = new ObjectMapper();
 
-    MDSConnector(Software software, List<VaccineGroup> forecastItemList) {
+    public MDSConnector(Software software, List<VaccineGroup> forecastItemList) {
         this.software = software;
         // throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
@@ -48,6 +48,7 @@ public class MDSConnector implements ConnectorInterface {
     }
 
     public List<ForecastActual> queryForForecast(TestCase testCase, SoftwareResult softwareResult) throws Exception {
+    	softwareResult.setSoftware(this.software);
         String gender = testCase.getPatientSex();
         Date dob = testCase.getPatientDob();
         Date evalDate = testCase.getEvalDate();
@@ -74,15 +75,17 @@ public class MDSConnector implements ConnectorInterface {
         	
             Careplan careplan = client.getForecast(this.software.getServiceUrl(), patient, evalDate, doses, writer);
             this.setLogText(true);
-            System.out.println(writer.toString());
-            ObjectMapper mapper = new ObjectMapper();
-            System.out.println(mapper.writeValueAsString(careplan)); 
+            writer.write("\n[Reading Evaluations]\n");
+            readEvalution(careplan, eventsMap, softwareResult);
+            writer.write("\n[Reading Forecasts]\n");
+            List<ForecastActual> forecast = readForecast(careplan, writer);
+            
             softwareResult.setLogText(writer.toString());
-            readEvalution(careplan, eventsMap);
-            List<ForecastActual> forecast = readForecast(careplan);
             return forecast;
             
         } catch(Exception exp) {
+        	writer.write("\nProblem Encountred [StackTrace] : \n");
+        	writer.write(exp.getMessage());
         	exp.printStackTrace();
         	this.setLogText(true);
         	softwareResult.setSoftwareResultStatus(SoftwareResultStatus.PROBLEM);
@@ -91,7 +94,7 @@ public class MDSConnector implements ConnectorInterface {
         }
     }
 
-    private List<ForecastActual> readForecast(Careplan careplan) {
+    private List<ForecastActual> readForecast(Careplan careplan, StringWriter log) throws JsonProcessingException {
         List<ForecastActual> actual = new ArrayList<ForecastActual>();
 
         for (Antigen antigen : careplan.getAntigen()) {
@@ -101,8 +104,14 @@ public class MDSConnector implements ConnectorInterface {
                 f.setDueDate(xmlToDate(series.getRecommendeddate()));
                 f.setOverdueDate(xmlToDate(series.getPastduedate()));
                 f.setValidDate(xmlToDate(series.getMindate()));
-                f.setVaccineCvx(findCvx(antigen.getName(), series.getAllowableCvx()));
+                if(series.getRecommendedCvx() != null || series.getAllowableCvx() != null) {
+                	f.setVaccineCvx(findCvx(antigen.getName(), series.getRecommendedCvx() != null ? series.getRecommendedCvx() : series.getAllowableCvx()));
+                } else {
+                    log.write("\nFound Forecast for - antigen : "+antigen.getName()+" - series : "+series.getName()+" without CVX");
+                	continue;
+                }
                 f.setFinishedDate(xmlToDate(series.getLatedate()));
+                log.write("\nFound Forecast for - antigen : "+antigen.getName()+" - series : "+series.getName()+" - CVX : "+f.getVaccineCvx());
                 actual.add(f);
             }
         }
@@ -110,72 +119,48 @@ public class MDSConnector implements ConnectorInterface {
         return actual;
     }
 
-    public static void main(String[] args) throws Exception {
-    	Software sft = new Software();
-        sft.setService(Service.MDS);
-        sft.setServiceUrl("http://testws.swpartners.com/mdsservice/mds");
-        
-        MDSConnector connector = new MDSConnector(sft,  null);
-        SoftwareResult sftr = new SoftwareResult();
-        sftr.setSoftware(sft);
-        
-        TestCase tc = new TestCase();
-        tc.setPatientSex("F");
-        tc.setPatientDob(sdf.parse("09/01/2011"));
-        tc.setEvalDate(sdf.parse("09/01/2018"));
-
-        TestEvent te1 = new TestEvent();
-        te1.setEvent(Event.getEvent(140));
-        te1.setEventDate(sdf.parse("09/01/2018"));
-        
-//        TestEvent te2 = new TestEvent();
-//        te2.setEvent(Event.getEvent(107));
-//        te2.setEventDate(sdf.parse("04/24/2018"));
-
-        tc.setTestEventList(Arrays.asList(te1));
-
-        
-
-        List<ForecastActual> fa = connector.queryForForecast(tc, sftr);
-//        System.out.println(fa);
-//        System.out.println(sftr.getLogText());
-        ObjectMapper mapper = new ObjectMapper();
-        System.out.println(mapper.writeValueAsString(fa));
-        System.out.println(mapper.writeValueAsString(tc.getTestEventList()));
-
-    }
-
     private List<String> allowable(String allowable) {
         return Arrays.asList(allowable.split(","));
     }
 
-    private void readEvalution(Careplan careplan, Map<Integer, Map<Long, TestEvent>> eventsMap) {
+    private void readEvalution(Careplan careplan, Map<Integer, Map<Long, TestEvent>> eventsMap, SoftwareResult softwareResult) throws JsonProcessingException {
         for (Antigen antigen : careplan.getAntigen()) {
             for (Series series : antigen.getSeries()) {
                 for (SeriesDose dose : series.getSeriesDose()) {   
-                    if (!dose.getStatus().equals("Skipped") && eventsMap.containsKey(Integer.parseInt(dose.getCvx())) && eventsMap.get(Integer.parseInt(dose.getCvx())).containsKey(xmlToDate(dose.getAdmindate()).getTime())) {
+                	
+                    if (dose.getCvx() != null && !dose.getStatus().equals("Skipped") && eventsMap.containsKey(Integer.parseInt(dose.getCvx())) && eventsMap.get(Integer.parseInt(dose.getCvx())).containsKey(xmlToDate(dose.getAdmindate()).getTime())) {
                         TestEvent event = eventsMap.get(Integer.parseInt(dose.getCvx())).get(xmlToDate(dose.getAdmindate()).getTime());
                         EvaluationActual evaluationActual = new EvaluationActual();
-	                    evaluationActual.setTestEvent(event);
-
-                        Evaluation ev = Evaluation.getEvaluationByLabel(dose.getStatus());
-                        if (ev == null && "Invalid".equalsIgnoreCase(dose.getStatus())) {
-                            evaluationActual.setEvaluation(Evaluation.NOT_VALID);
+                        evaluationActual.setSoftwareResult(softwareResult);
+                        evaluationActual.setTestEvent(event);
+                        evaluationActual.setDoseNumber(dose.getDosenum() != null ? dose.getDosenum()+"" : null);
+                        evaluationActual.setEvaluation(evaluationFromString(dose.getStatus()));
+                        evaluationActual.setDoseValid(evaluationActual.getEvaluation() == Evaluation.VALID ? "Y" : "N");
+                        evaluationActual.setVaccineCvx(findCvx(antigen.getName(), dose.getCvx()));
+                        if(series.getRecommendedCvx() != null || series.getAllowableCvx() != null) {
+                        	evaluationActual.setVaccineCvx(findCvx(antigen.getName(), series.getRecommendedCvx() != null ? series.getRecommendedCvx() : series.getAllowableCvx()));
                         } else {
-                            evaluationActual.setEvaluation(ev);
+                        	evaluationActual.setVaccineCvx(findCvx(antigen.getName(), dose.getCvx()));
                         }
-
-                        if (event.getEvaluationActualList() == null) {
-                            event.setEvaluationActualList(new ArrayList<EvaluationActual>());
-                        }
-
-                        evaluationActual.setVaccineCvx(findCvx(antigen.getName(), series.getAllowableCvx()));
                         evaluationActual.setSeriesUsedCode(evaluationActual.getVaccineCvx());
+
+                        if(event.getEvaluationActualList() == null) {
+                        	event.setEvaluationActualList(new ArrayList<>());
+                        }
                         event.getEvaluationActualList().add(evaluationActual);
                     }
                 }
             }
         }
+    }
+    
+    private Evaluation evaluationFromString(String evaluation) {
+    	Evaluation ev = Evaluation.getEvaluationByLabel(evaluation);
+    	if(ev == null || "Invalid".equalsIgnoreCase(evaluation)) {
+    		return Evaluation.NOT_VALID;
+    	} else {
+    		return ev;
+    	}
     }
 
     private String findCvx(String antigen, String allowableStr) {
